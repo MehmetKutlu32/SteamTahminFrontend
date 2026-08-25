42e8795a419da12dc904b915e8f2b2ff442a6e2d
Mehmet Kutlu <mehmet.kutlu.0620@gmail.com>
2026-08-25 13:54:40 +0300

    release: v1.0.2+3 with transparent update indicator and responsive keyboard layout in 1v1 duel

--- lib/services/update_service.dart
+++ lib/services/update_service.dart
@@ -20,7 +20,7 @@ class UpdateService extends ChangeNotifier {
   bool get isDownloading => _state == AppUpdateState.downloading;
   bool get isUpdateReady => _state == AppUpdateState.readyToRestart;
 
-  static const String appVersion = 'v1.0.1';
+  static const String appVersion = 'v1.0.2';
 
   int? _currentPatch;
   int? get currentPatch => _currentPatch;
